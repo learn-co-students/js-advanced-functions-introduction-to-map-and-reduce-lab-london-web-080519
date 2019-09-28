5e319f2b9453f3a4b041f5c9e4b2cf443fc0157a
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-28 14:24:42 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let answer = []
+    for (let i=0; i<sourceArray.length; i++) {
+        answer.push(sourceArray[i] * -1)
+    }
+    return answer
+}
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+function mapToDouble(sourceArray) {
+    let answer = []
+    for (let i=0; i<sourceArray.length; i++) {
+        answer.push(sourceArray[i] * 2)
+    }
+    return answer
+}
+function mapToSquare(sourceArray) {
+    let answer = []
+    for (let i=0; i<sourceArray.length; i++) {
+        answer.push(sourceArray[i] * sourceArray[i])
+    }
+    return answer
+}
+
+function reduceToTotal(sourceArray, startingPoint) {
+    let memo = 0
+    for (let i=0 ; i< sourceArray.length; i++) {
+        memo += sourceArray[i]
+    } 
+    return (startingPoint ? startingPoint + memo : memo )
+}
+
+function reduceToAllTrue(sourceArray) {
+    let memo = true
+    for (let i=0; i<sourceArray.length; i++) {
+        if (!sourceArray[i]) {
+            memo = false
+        }
+    }
+    return memo
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let memo = false
+    for (let i=0; i<sourceArray.length; i++) {
+        if (!!sourceArray[i]) {
+            memo = true
+        }
+    }
+    return memo
+}
